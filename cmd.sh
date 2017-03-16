cd7fd4088483e706e895c1c47f68aa0b88e73338
zhourunlai <xiaorun95@gmail.com>
2017-03-16 12:27:45 +0800

    update

--- cmd.sh
+++ cmd.sh
@@ -1,9 +1,5 @@
 cd $GOPATH/src/
-//安装beego
-go get github.com/astaxie/beego
-//安装bee工具
-go get github.com/beego/bee
 //下载项目
 git clone https://github.com/zhourunlai/message.git & cd message/
 //启动
-nohup bee run -downdoc=true -gendoc=true &
+nohup bee run &
