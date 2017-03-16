f01f106b8b2cb7cf4a8ba00fbaaec10861335a36
zhourunlai <xiaorun95@gmail.com>
2017-03-16 14:11:33 +0800

    update

--- views/src/components/signup.vue
+++ views/src/components/signup.vue
@@ -4,7 +4,22 @@ import { actions } from '../store';
 export default {
     vuex: {
         actions: actions,
-    },
+        data:{
+            this.$http.get('/v1/user/signup', {
+                params: {
+                    username: this.username,
+                    password: this.password,
+
+                }
+            }).then((response) => {
+                transition.next({
+                    data: response.json().data,
+                })
+            }, (response) => {
+                // error
+                })
+            }
+        },
 };
 </script>
 
